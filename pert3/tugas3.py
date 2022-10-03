56d37dadd4fca19e049e6f853b451398fc89d358
root <root@DESKTOP-VLGHPPB.localdomain>
2022-10-03 08:19:24 -0700

    tugas 3

---
+++ pert3/tugas3.py
@@ -0,0 +1,20 @@
+#menampilkan nama dan NIM
+print('Nama: Restu Eka Putri')
+print('NIM: 20210801042')
+
+#program operator + - x
+a = 10
+b = 5
+print('a + b =', a+b)
+print('a - b =', a-b)
+print('a * b =', a*b)
+print('a / b =', a/b)
+
+## MATRIX
+
+#Matriks dengan ukuran 2x2
+matriksA = [ [2,0], [0,2] ]
+matriksB = [ [10,5], [20,6]]
+#Menampilkan matriks dengan nama matriksA
+print (matriksA)
+print (matriksB)

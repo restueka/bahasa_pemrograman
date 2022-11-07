bf97b9fcfae3bbc9db5bdaac66519135bb4b34bc
restueka <restueka007ok@gmail.com>
2022-11-06 18:51:49 -0800

    Array

---
+++ pert6/sample.py
@@ -0,0 +1,17 @@
+# definisi function
+
+#header
+def kepala():
+    print ("NAMASAYA R")
+    print ("NIMSAYA 7")
+    print ("----------")
+#body
+def penjumlahan():
+    print("masukan angka 1 : ")
+    print("masukan angka 2 :  ")
+#footer
+def kaki():
+    print ("Terima Kasih")
+
+while True:
+    kepala()
---
+++ pert7/looparray.py
@@ -0,0 +1,26 @@
+import array as arr 
+
+numbers = arr.array('i',[10,20,30])
+
+print(numbers)
+
+import array as arr 
+
+numbers = arr.array('i',[10,20,30])
+
+for number in numbers:
+    print(number)
+
+
+import array as arr  
+
+values = arr.array('i',[10,20,30])
+
+#prints each individual value in the array
+for value in range(len(values)):
+    print(values[value])
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Pr

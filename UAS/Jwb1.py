617521659575c69ab316f7ecb7bd710e14d094cd
restueka <114573239+restueka@users.noreply.github.com>
2023-01-23 04:04:10 -0800

    Create Jwb1.py

---
+++ UAS/Jwb1.py
@@ -0,0 +1,12 @@
+def greet(name):
+    print("Hello, " + name + "!")
+  
+greet("Restu")
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else;
+         return n * factorial(n-1)
+    
+print(factorial(4))
